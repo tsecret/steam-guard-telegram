e60124a3d9ca9529cf123c3e69d1a1a3667e32e9
Timur <36734601+TSecretT@users.noreply.github.com>
2021-12-21 18:56:35 +0100

    .

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import json
 import struct
 import time
 from hashlib import sha1
-
 import config
+import threading
 
 bot = telebot.TeleBot(config.TELEGRAM_BOT_API)
+
+activeMessages = []
 
 # Simple function that generates steam codes by passing shared secret key
 def getTwoFactorCode(shared_secret: str) -> str:
@@ -33,30 +35,42 @@ def loadAccounts() -> list:
         print("loadAccounts() error: ", err)
         return []
 
-def generateCodes(message) -> str:
+def generateCodes() -> str:
     try:
-        if message.chat.id not in config.WHITELIST: return "You are not in whitelist"
-
         accounts = loadAccounts()
         msg=""
 
-        for account in accounts:
-            if "shared_secret" in account:
-                msg = msg + f"{account['username']}: {getTwoFactorCode(account['shared_secret'])}\n"
+
+        for username in accounts:
+            if "shared_secret" in accounts[username]:
+                msg = msg + f"{username}: {getTwoFactorCode(accounts[username]['shared_secret'])}\n"
 
         return msg
     except Exception as e:
         print("Error: ", e)
         return "Error using the bot"
 
+def terminateMessage(chat_id, messages, terminationTime):
+    while int(time.time()) < terminationTime:
+        pass
+    for message in messages:
+        bot.delete_message(chat_id=chat_id, message_id=message)
+
 # Handling the incoming telegram message
 @bot.message_handler(commands=['start', 'menu'])
 def start(message):
-    bot.send_message(message.chat.id, generateCodes(message))
+    if message.chat.id not in config.WHITELIST:
+        bot.send_message(message.chat.id, "You are not in whitelist")
+
+    msg = generateCodes()
+    botmsg = bot.send_message(message.chat.id, msg)
+    threading.Thread(target=terminateMessage, args=(message.chat.id, [botmsg.message_id, message.message_id], time.time() + config.DELETE_TIME)).start()
 
 while True:
     try:
         print("Bot running")
         bot.polling(none_stop=True, timeout=15)
+    except KeyboardInterrupt:
+        exit()
     except Exception as e:
         time.sleep(20)
